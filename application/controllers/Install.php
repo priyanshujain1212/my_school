b1ac59fdfcdbdaec866be6950e63a7be2f1142c6
Priyanshu Jain <priyanshujain1212@gmail.com>
2020-07-03 14:19:00 +0530

    Remove Purchase Code

--- application/controllers/Install.php
+++ application/controllers/Install.php
@@ -102,29 +102,30 @@ class Install extends CI_Controller
 
     function verify_purchase($purchase_code = '') 
     {
-        $product_code = $purchase_code;
-        $personal_token = "fVeXFGrqey5861iUpCuVrZ8tBqf85JmS";
-        $url = "https://api.envato.com/v3/market/author/sale?code=".$product_code;
-        $curl = curl_init($url);
-        $bearer   = 'bearer ' . $personal_token;
-        $header   = array();
-        $header[] = 'Content-length: 0';
-        $header[] = 'Content-type: application/json; charset=utf-8';
-        $header[] = 'Authorization: ' . $bearer;
-        $verify_url = 'https://api.envato.com/v1/market/private/user/verify-purchase:'.$product_code.'.json';
-        $ch_verify = curl_init( $verify_url . '?code=' . $product_code );
-        curl_setopt( $ch_verify, CURLOPT_HTTPHEADER, $header );
-        curl_setopt( $ch_verify, CURLOPT_SSL_VERIFYPEER, false );
-        curl_setopt( $ch_verify, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $ch_verify, CURLOPT_CONNECTTIMEOUT, 5 );
-        curl_setopt( $ch_verify, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-        $cinit_verify_data = curl_exec( $ch_verify );
-        curl_close( $ch_verify );
-        $response = json_decode($cinit_verify_data, true);
-        if (count($response['verify-purchase']) > 0) {
-            return true;
-        } else {
-            return false;
-        }
+        // $product_code = $purchase_code;
+        // $personal_token = "fVeXFGrqey5861iUpCuVrZ8tBqf85JmS";
+        // $url = "https://api.envato.com/v3/market/author/sale?code=".$product_code;
+        // $curl = curl_init($url);
+        // $bearer   = 'bearer ' . $personal_token;
+        // $header   = array();
+        // $header[] = 'Content-length: 0';
+        // $header[] = 'Content-type: application/json; charset=utf-8';
+        // $header[] = 'Authorization: ' . $bearer;
+        // $verify_url = 'https://api.envato.com/v1/market/private/user/verify-purchase:'.$product_code.'.json';
+        // $ch_verify = curl_init( $verify_url . '?code=' . $product_code );
+        // curl_setopt( $ch_verify, CURLOPT_HTTPHEADER, $header );
+        // curl_setopt( $ch_verify, CURLOPT_SSL_VERIFYPEER, false );
+        // curl_setopt( $ch_verify, CURLOPT_RETURNTRANSFER, 1 );
+        // curl_setopt( $ch_verify, CURLOPT_CONNECTTIMEOUT, 5 );
+        // curl_setopt( $ch_verify, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+        // $cinit_verify_data = curl_exec( $ch_verify );
+        // curl_close( $ch_verify );
+        // $response = json_decode($cinit_verify_data, true);
+        // if (count($response['verify-purchase']) > 0) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return true;
     }
 }
